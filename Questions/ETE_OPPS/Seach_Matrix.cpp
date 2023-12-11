c8f2819871ff75ac3e3e100f5ba19b37ecf8894a
Khushi <khushijamloki858@gmail.com>
2023-12-11 20:30:26 +0530

    17/12

---
+++ Questions/ETE_OPPS/Seach_Matrix.cpp
@@ -0,0 +1,11 @@
+//
+// Created by khushi on 12/10/2023.
+//
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int r , c;
+    cin>>r>>c;
+    for(int i = 0; i<r;i++)
+    return 0;
+}
